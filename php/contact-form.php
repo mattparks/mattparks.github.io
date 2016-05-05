964e963213efd806f4cc968c468ce708703a0cb7
Matthew Albrecht <mattparks5855@gmail.com>
2016-05-05 09:11:55 -0500

    Lots of small changes, added charts and trying to make email work.

--- php/contact-form.php
+++ php/contact-form.php
@@ -3,29 +3,25 @@
   ini_set('display_errors', 'On');
   set_error_handler("var_dump");
 
-  $email_to = "mattparks5855@gmail.com";
-  $name = Trim(stripslashes($_POST['Name']));
-  $email_from = Trim(stripslashes($_POST['Email']));
-  $subject = "Mattparks Contact: " . Trim(stripslashes($_POST['Subject']));
-  $message = Trim(stripslashes($_POST['Message']));
+  $from = 'no-reply@mywebsite.com';
+  $from_name = 'Mattparks Contact';
+  $email_to = 'mattparks5855@gmail.com';
+  $name = $_POST['Name'];
+  $email_from = $_POST['Email'];
+  $subject = '[Mattparks Contact] ' . $_POST['Subject'];
+  $message = $_POST['Message'];
 
   $body  = "Name: " . $name . "\n";
   $body .= "Email: " . $email_from . "\n";
   $body .= "Subject: " . $subject . "\n" . "\n";
   $body .= $message . "\n";
-  echo $body;
 
-  $headers  = "MIME-Version: 1.1" . PHP_EOL;
-  $headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
-  $headers .= "Content-Transfer-Encoding: 8bit" . PHP_EOL;
-  $headers .= "Date: " . date('r', $_SERVER['REQUEST_TIME']) . PHP_EOL;
-  $headers .= "Message-ID: <" . $_SERVER['REQUEST_TIME'] . md5($_SERVER['REQUEST_TIME']) . '@' . $_SERVER['SERVER_NAME'] . '>' . PHP_EOL;
-  $headers .= "From: " . "=?UTF-8?B?".base64_encode($name)."?=" . "<$email_from>" . PHP_EOL;
-  $headers .= "Return-Path: $email_to" . PHP_EOL;
-  $headers .= "Reply-To: $email_from" . PHP_EOL;
-  $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
-  $headers .= "X-Originating-IP: " . $_SERVER['SERVER_ADDR'] . PHP_EOL;
-  $sent = mail($email_to, "=?utf-8?B?".base64_encode($subject)."?=", $body, $headers);
-  echo "Message sent correctly: ";
-  echo ($sent) ? 'true' : 'false';
+  echo "Name: " . $name . "<br>";
+  echo "Email: " . $email_from . "<br>";
+  echo "Subject: " . $subject . "<br>";
+  echo "<br>" . $message . "<br>";
+
+  $sent = mail($email_to, $subject, $body, "From: " . $from_name . " <".$from.">", "-f $from -r " . $email_to);
+  echo "<br>" . "Email Sent: ";
+  echo ($sent) ? "True" : "False";
 ?>
